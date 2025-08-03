8c5d79fd44df81e8be7cd81faf188892313f19bf
B1Fr0st <lovewolves413@gmail.com>
2025-08-02 21:17:39 -0700

    Testing + github actions

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,19 @@ use serde_derive::Deserialize;
 
 
 #[derive(Debug)]
+/// The main struct for the Dumpspace API, which provides methods to interact with the Dumpspace data.
+/// It must be initialized with a specific game ID (hash) and then must call `download_content` to fetch and parse the data.
+/// # Example:
+/// ```
+/// use dumpspace_api::DSAPI;
+/// let game_id = "6b77eceb"; // Example game ID, replace with actual game hash
+/// let mut dsapi = DSAPI::new(game_id);
+/// dsapi.download_content().unwrap(); // Download and parse the content (if this fails you're screwed anyways so might as well unwrap)
+/// println!("{:?}", dsapi.get_member_offset("UWorld", "OwningGameInstance"));
+/// println!("{:?}", dsapi.get_enum_name("EFortRarity", 4));
+/// println!("0x{:x?}", dsapi.get_class_size("AActor").unwrap());
+/// println!("0x{:x?}", dsapi.get_offset("OFFSET_GWORLD").unwrap());
+/// ```
 pub struct DSAPI {
     game_list: GameList,
     class_member_map: HashMap<String, OffsetInfo>,
@@ -20,6 +33,9 @@ pub struct DSAPI {
 }
 
 impl DSAPI {
+    /// Creates a new instance of `DSAPI` for a specific game identified by its hash.
+    /// This function initializes the game list and sets the engine and location based on the provided game ID.
+    /// Game ID can be found in the url of a dumpspace game page, and will be a query argument called `hash`.
     pub fn new(game_id: &str) -> Self {
         let mut ret = DSAPI {
             game_list: GameList::init().expect("Failed to initialize game list"),
@@ -41,7 +57,9 @@ impl DSAPI {
             .clone();
         ret
     }
-
+    /// Downloads and parses the content from the dumpspace API.
+    /// This function fetches various JSON blobs containing class, struct, enum, and function information,
+    /// and populates the internal maps with this data.
     pub fn download_content(&mut self) -> Result<(), String> {
         fn parse_class_info(classes_info: &BlobInfo, dsapi: &mut DSAPI) {
             for class in &classes_info.data {
@@ -150,21 +168,20 @@ impl DSAPI {
         }
 
 
-        let url = format!("{}/{}/{}/FunctionsInfo.json.gz", base_url, self.engine, self.location);
-        let resp = download_url(&url)
-            .expect("Failed to download functions info"); 
-        let functions_info = serde_json::from_str::<BlobInfo>(&resp)
-            .expect("Failed to parse functions info");
-
-        for function in &functions_info.data {
-            continue;
-            for (key, value) in function {
-                dbg!(key, value);
-                let function_name = key;
-                let value = value.as_array().unwrap()[2].as_u64().unwrap();
-                self.function_offset_map.insert(function_name.clone() + &function_name, value);
-            }
-        }
+        // let url = format!("{}/{}/{}/FunctionsInfo.json.gz", base_url, self.engine, self.location);
+        // let resp = download_url(&url)
+        //     .expect("Failed to download functions info"); 
+        // let functions_info = serde_json::from_str::<BlobInfo>(&resp)
+        //     .expect("Failed to parse functions info");
+        // for function in &functions_info.data {
+            
+        //     for (key, value) in function {
+        //         dbg!(key, value);
+        //         let function_name = key;
+        //         let value = value.as_array().unwrap()[2].as_u64().unwrap();
+        //         self.function_offset_map.insert(function_name.clone() + &function_name, value);
+        //     }
+        // }
 
 
         let url = format!("{}/{}/{}/OffsetsInfo.json.gz", base_url, self.engine, self.location);
@@ -182,22 +199,36 @@ impl DSAPI {
 
         Ok(())
     }
-
+    /// Returns the offset info for a class member as an `Option<OffsetInfo>`.
     pub fn get_member_offset(&self, class_name: &str, member_name: &str) -> Option<OffsetInfo> {
         self.class_member_map.get(&(class_name.to_string() + member_name)).cloned()
     }
+    /// Returns the size of a class as an `Option<i32>`.
+    /// Returns `None` if the class is not found.
     pub fn get_class_size(&self, class_name: &str) -> Option<i32> {
         self.class_size_map.get(class_name).cloned()
     }
+    /// Returns the offset of a function as an `Option<u64>`.
+    /// Returns `None` if the function is not found.
+    /// Note: Functions are not currently implemented.
+    #[allow(dead_code)] //removeme
     fn get_function_offset(&self, function_class: &str, function_name: &str) -> Option<u64> {
         self.function_offset_map.get(&(function_class.to_string() + function_name)).cloned()
     }
+    /// Returns the name of an enum value as an `Option<String>`.
+    /// Returns `None` if the enum name or value is not found.
     pub fn get_enum_name(&self, enum_name: &str, enum_value: i64) -> Option<String> {
         self.enum_name_map.get(&(enum_name.to_string() + &enum_value.to_string())).cloned()
     }
+    /// Returns the offset of a specific offset name as an `Option<u64>`.
+    /// Returns `None` if the offset name is not found.
     pub fn get_offset(&self, offset_name: &str) -> Option<u64> {
         self.offset_map.get(offset_name).cloned()
     }
+    /// Returns the offset info for a class member with an .unwrap() and cast to usize.
+    /// This function will panic if the member is not found.
+    /// # Safety: This function assumes that the member exists and will panic if it does not.
+    /// This should be fine to use in practice, as the code should only panic if the member is misspelled or does not exist.
     pub fn get_member_offset_unchecked(&self, class_name: &str, member_name: &str) -> usize {
         self.class_member_map.get(&(class_name.to_string() + member_name)).cloned().unwrap().offset as usize
     }
@@ -211,7 +242,6 @@ pub struct GameList {
 
 
 #[derive(Deserialize, Debug)]
-#[allow(dead_code)]
 pub struct Game {
     pub hash: String,
     pub name: String,
@@ -222,7 +252,6 @@ pub struct Game {
 }
 
 #[derive(Deserialize, Debug)]
-#[allow(dead_code)]
 pub struct Uploader {
     pub name: String,
     pub link: String,
@@ -272,7 +301,6 @@ struct OffsetBlob {
     updated_at: String, // Unix timestamp
     version: u64, // Version number
 }
-#[allow(dead_code)]
 impl GameList {
     pub fn init() -> Result<Self, String> {
         let url = "https://dumpspace.spuckwaffel.com/Games/GameList.json";
@@ -295,32 +323,96 @@ impl GameList {
     }
 }
 
-// fn main() {
+#[cfg(test)]
+mod tests {
+    use super::*;
+    static mut LOCAL_DSAPI: std::sync::LazyLock<DSAPI> = std::sync::LazyLock::new(||{let mut res = DSAPI::new("6b77eceb");res.download_content().unwrap();return res;}); //fortnite
 
-//     let game_id = "6b77eceb"; // Example game ID, replace with actual game hash
-//     let mut dsapi = DSAPI::new(game_id);
+    #[test]
+    fn test_new_dsapi() {
+        let dsapi = DSAPI::new("6b77eceb");
+        assert_eq!(dsapi.engine, "Unreal-Engine-5");
+        assert_eq!(dsapi.location, "Fortnite");
+    }
 
-//     dsapi.download_content().unwrap_or_else(|err| {
-//         eprintln!("Error downloading content: {}", err);
-//         std::process::exit(1);
-//     });
+    #[test]
+    fn test_get_member_offset_some() {
+        let dsapi = unsafe{ (&raw const LOCAL_DSAPI).as_ref().unwrap() };
+        let info = dsapi.get_member_offset("UWorld", "OwningGameInstance");
+        assert!(info.is_some());
+        let info = info.unwrap();
+        assert_eq!(info.offset, 0x228);
+        assert_eq!(info.size, 8);
+        assert!(info.valid);
+    }
 
-//     println!("{:?}", dsapi.get_member_offset("UWorld", "OwningGameInstance"));
-    
-//     println!("{:?}", dsapi.get_enum_name("EFortRarity", 4));
+    #[test]
+    fn test_get_member_offset_none() {
+        let dsapi = unsafe{ (&raw const LOCAL_DSAPI).as_ref().unwrap() };
+        assert!(dsapi.get_member_offset("NoClass", "NoMember").is_none());
+    }
 
-//     println!("0x{:x?}", dsapi.get_class_size("AActor").unwrap());
+    #[test]
+    fn test_get_class_size_some() {
+        let dsapi = unsafe{ (&raw const LOCAL_DSAPI).as_ref().unwrap() };
+        assert_eq!(dsapi.get_class_size("UWorld"), Some(2536));
+    }
 
-//     println!("0x{:x?}", dsapi.get_offset("OFFSET_GWORLD").unwrap());
+    #[test]
+    fn test_get_class_size_none() {
+        let dsapi = unsafe{ (&raw const LOCAL_DSAPI).as_ref().unwrap() };
+        assert_eq!(dsapi.get_class_size("NoClass"), None);
+    }
 
-    
-//     // let game_list = GameList::init().unwrap_or_else(|err| {
-//     //     eprintln!("Error initializing game list: {}", err);
-//     //     std::process::exit(1);
-//     // });
+    #[test]
+    #[allow(unreachable_code)] //removeme
+    fn test_get_function_offset_some() {
+        return; //functions are not implemented yet.
+        let dsapi = unsafe{ (&raw const LOCAL_DSAPI).as_ref().unwrap() };
+        assert_eq!(dsapi.get_function_offset("TestClass", "TestFunc"), Some(0x1234));
+    }
 
-//     // println!("Game List: {:#?}", game_list.get_game_by_name("Fortnite").unwrap_or_else(|| {
-//     //     eprintln!("Game not found");
-//     //     std::process::exit(1);
-//     // }).get_classes());
-// }
+    #[test]
+    fn test_get_function_offset_none() {
+        let dsapi = unsafe{ (&raw const LOCAL_DSAPI).as_ref().unwrap() };
+        assert_eq!(dsapi.get_function_offset("NoClass", "NoFunc"), None);
+    }
+
+    #[test]
+    fn test_get_enum_name_some() {
+        let dsapi = unsafe{ (&raw const LOCAL_DSAPI).as_ref().unwrap() };
+        assert_eq!(dsapi.get_enum_name("EFortRarity", 1), Some("EFortRarity__Uncommon".to_string()));
+    }
+
+    #[test]
+    fn test_get_enum_name_none() {
+        let dsapi = unsafe{ (&raw const LOCAL_DSAPI).as_ref().unwrap() };
+        assert_eq!(dsapi.get_enum_name("NoEnum", 2), None);
+    }
+
+    #[test]
+    fn test_get_offset_some() {
+        let dsapi = unsafe{ (&raw const LOCAL_DSAPI).as_ref().unwrap() };
+        assert_eq!(dsapi.get_offset("OFFSET_GWORLD"), Some(0x14942840));
+    }
+
+    #[test]
+    fn test_get_offset_none() {
+        let dsapi = unsafe{ (&raw const LOCAL_DSAPI).as_ref().unwrap() };
+        assert_eq!(dsapi.get_offset("NO_OFFSET"), None);
+    }
+
+    #[test]
+    fn test_get_member_offset_unchecked() {
+        let dsapi = unsafe{ (&raw const LOCAL_DSAPI).as_ref().unwrap() };
+        let offset = dsapi.get_member_offset_unchecked("UWorld", "OwningGameInstance");
+        assert_eq!(offset, 0x228);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_get_member_offset_unchecked_panic() {
+        let dsapi = unsafe{ (&raw const LOCAL_DSAPI).as_ref().unwrap() };
+        dsapi.get_member_offset_unchecked("NoClass", "NoMember");
+    }
+}
